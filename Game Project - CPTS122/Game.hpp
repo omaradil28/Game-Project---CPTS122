12d50a1764f87ad5e3e053f9f0f4eb67f06960e6
omaradil28 <omar.adil9492@gmail.com>
2024-04-15 22:03:03 -0700

    Update Game.hpp

--- Game Project - CPTS122/Game.hpp
+++ Game Project - CPTS122/Game.hpp
@@ -160,7 +160,7 @@ void Game::runGame() {
             platform.generatePlatform();
             clock.restart();
         }
-        platform.movePlatforms(0.2f); 
+        platform.movePlatforms(0.5f); 
         for (auto& platformSprite : platform.getPlatforms()) {
             Play.draw(platformSprite);
         }
