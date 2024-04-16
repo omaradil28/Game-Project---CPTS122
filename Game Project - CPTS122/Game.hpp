52e0f1a824ea797fb8c45a92dda3ba80af332053
Alejandro Ramirez <alramirez9924@gmail.com>
2024-04-16 16:54:25 -0700

    Window Size Change
    Window will now open to max screen resolution. This helps for running on different systems.

--- Game Project - CPTS122/Game.hpp
+++ Game Project - CPTS122/Game.hpp
@@ -8,6 +8,7 @@
 #include"Platform.hpp"
 
 #define WinWidth VideoMode().getDesktopMode().width // Window Height and Width
+#define WinHeight VideoMode().getDesktopMode().height
 
 class Game {
 public:
